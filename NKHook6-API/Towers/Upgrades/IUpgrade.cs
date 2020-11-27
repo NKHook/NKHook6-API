b85ab33e8f0472b023ef8d7be6262aecf83f2fd4
DisabledMallis <mallisiscool@gmail.com>
2020-11-27 13:48:21 -0500

    Update IUpgrade.cs

--- NKHook6-API/Towers/Upgrades/IUpgrade.cs
+++ NKHook6-API/Towers/Upgrades/IUpgrade.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace NKHook6.API.Towers.Upgrades
 {
+    /// <summary>
+    /// An interface for working with Upgrades
+    /// </summary>
     public interface IUpgrade
     {
+        /// <summary>
+        /// Get the cost of the upgrade
+        /// </summary>
+        /// <returns>The cost of the upgrade</returns>
+        double getCost();
+        /// <summary>
+        /// Get the name of the upgrade (Non localized)
+        /// </summary>
+        /// <returns>The name of the upgrade</returns>
+        string getName();
+        /// <summary>
+        /// Get the description of the upgrade
+        /// </summary>
+        /// <returns>The upgrade description</returns>
+        string getDescription();
     }
 }
