ae9e7c60f4731bc330dffa481d48b99f8f90ff2c
DisabledMallis <mallisiscool@gmail.com>
2020-12-01 14:59:30 -0500

    Update MoveEvent.cs

--- NKHook6-API/Events/Bloons/MoveEvent.cs
+++ NKHook6-API/Events/Bloons/MoveEvent.cs
@@ -7,14 +7,10 @@ namespace NKHook6.API.Events._Bloons
         public class MoveEvent : EventBaseCancellable
         {
             public IBloonEntity bloon;
-            public float newPosition;
-            public float oldPosition;
 
-            public MoveEvent(IBloonEntity bloon, float newPosition, float oldPosition) : base("BloonMoveEvent")
+            public MoveEvent(IBloonEntity bloon) : base("BloonMoveEvent")
             {
                 this.bloon = bloon;
-                this.newPosition = newPosition;
-                this.oldPosition = oldPosition;
             }
         }
     }
